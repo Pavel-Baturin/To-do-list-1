d5f02f98914d1e9293aaa3c1d405676abd5f761c
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-12-16 08:29:48 +0200

    Добавляет начальную разметку

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -1,11 +1,19 @@
-import { DB_TO_DO_LIST } from "./const.js";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, onValue} from "firebase/database";
+
+import { DB_TO_DO_LIST, FIREBASE_CONFIG, PATH } from "./const.js";
+
+const app = initializeApp(FIREBASE_CONFIG);
+const db = getDatabase();
+console.log(app);
 
 const inputRef = document.querySelector("input");
 const buttonRef = document.querySelector("button");
 const listRef = document.querySelector("#myUL");
 
 listRef.addEventListener('click', onListClick);
-
 buttonRef.addEventListener('click', handlerClick);
 
 function handlerClick() {
@@ -16,42 +24,54 @@ function handlerClick() {
     saveData(value)
     creatMurkup();
 }
+
 creatMurkup();
 function creatMurkup() {
     let arrayData = getData();
-    if (!arrayData.length) {
-        return
+    //     if (!arrayData.length) {
+    //         return
+    //     }
+    //     const murkup = arrayData.map(({ id, value, approved }) => {
+    //         const check = approved ? 'checked' : "";
+    //         return `<li class="${check}"data-id="${id}">${value}<li>`;
+    //     });
+    //     console.log(murkup);
+    //     listRef.innerHTML = murkup.join('');
     }
-    const murkup = arrayData.map(({ id, value, approved }) => {
-        const check = approved ? 'checked' : "";
-        return `<li class="${check}"data-id="${id}">${value}<li>`;
-    });
-    console.log(murkup);
-    listRef.innerHTML = murkup.join('');
-    
-}
 
-function saveData(value) {
-    let arrayData = getData();
-   
-    const data = {
-        value,
-        approved: false,
-        id: Date.now(),
-    }
-    arrayData.push(data);
-    localStorage.setItem(DB_TO_DO_LIST, JSON.stringify(arrayData))
-}
-function getData() {
-    const data = localStorage.getItem(DB_TO_DO_LIST);
-    return !data ? [] : JSON.parse(data);
-}
+    function saveData(value) {
+        // let arrayData = getData();
+        const time = Date.now();
+        const data = {
+            value,
+            approved: false,
+            id: time,
+        }
 
-function onListClick(event) {
-    if (event.target.tagName !== 'LI') {
-        return
+        function writeUserData(userId, name, email, imageUrl) {
+            set(ref(db, 'listItems/' + time), data);
+        }
+        // arrayData.push(data);
+        // localStorage.setItem(DB_TO_DO_LIST, JSON.stringify(arrayData))
     }
-    let arrayData = getData();
+
+    function getData() {
+        // const data = localStorage.getItem(DB_TO_DO_LIST);
+        // return !data ? [] : JSON.parse(data);
+        const promise = new Promise((resolve, reject) => {  onValue(ref(db, PATH), (snapshot) => {
+            const data = snapshot.val();
+        });})
+        
+      
+        const items = Object.values(data);
+    }
+
+    function onListClick(event) {
+        if (event.target.tagName !== 'LI') {
+            return
+        }
+        let arrayData = getData();
     
-    console.log(event.target.dataset.id)
-}
+        console.log(event.target.dataset.id)
+    }
+
--- src/js/const.js
+++ src/js/const.js
@@ -1,1 +1,12 @@
 export const DB_TO_DO_LIST = "db-to-do-list";
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA75S0DJiARX3pgtH-3wSVZYD4RsY0liZI",
+  authDomain: "to-do-list-685c4.firebaseapp.com",
+  databaseURL: "https://to-do-list-685c4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "to-do-list-685c4",
+  storageBucket: "to-do-list-685c4.appspot.com",
+  messagingSenderId: "30257753670",
+  appId: "1:30257753670:web:d151b4b13c6d90d68d6447"
+};
+export const PATH = 'listItems/';
