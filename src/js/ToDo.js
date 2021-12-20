9c37f62775077002a5f25cd0284b83b2f797a91b
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-12-20 10:31:40 +0200

    добавляет элементы работы с firebase

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getDatabase, ref, set } from "firebase/database";
-import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref, set, onValue, remove, update } from "firebase/database";
+// import { getDatabase, ref, onValue} from "firebase/database";
 
 import { DB_TO_DO_LIST, FIREBASE_CONFIG, PATH } from "./const.js";
-
+import { inputRef, buttonRef, listRef } from "./refs.js";
 const app = initializeApp(FIREBASE_CONFIG);
 const db = getDatabase();
 console.log(app);
 
-const inputRef = document.querySelector("input");
-const buttonRef = document.querySelector("button");
-const listRef = document.querySelector("#myUL");
+
 
 listRef.addEventListener('click', onListClick);
 buttonRef.addEventListener('click', handlerClick);
@@ -22,56 +19,75 @@ function handlerClick() {
       return  
     }
     saveData(value)
-    creatMurkup();
+    createMarkup();
 }
 
-creatMurkup();
-function creatMurkup() {
-    let arrayData = getData();
-    //     if (!arrayData.length) {
-    //         return
-    //     }
-    //     const murkup = arrayData.map(({ id, value, approved }) => {
-    //         const check = approved ? 'checked' : "";
-    //         return `<li class="${check}"data-id="${id}">${value}<li>`;
-    //     });
-    //     console.log(murkup);
-    //     listRef.innerHTML = murkup.join('');
-    }
 
-    function saveData(value) {
-        // let arrayData = getData();
-        const time = Date.now();
-        const data = {
-            value,
-            approved: false,
-            id: time,
-        }
-
-        function writeUserData(userId, name, email, imageUrl) {
-            set(ref(db, 'listItems/' + time), data);
-        }
-        // arrayData.push(data);
-        // localStorage.setItem(DB_TO_DO_LIST, JSON.stringify(arrayData))
-    }
-
-    function getData() {
-        // const data = localStorage.getItem(DB_TO_DO_LIST);
-        // return !data ? [] : JSON.parse(data);
-        const promise = new Promise((resolve, reject) => {  onValue(ref(db, PATH), (snapshot) => {
-            const data = snapshot.val();
-        });})
-        
-      
-        const items = Object.values(data);
-    }
-
-    function onListClick(event) {
-        if (event.target.tagName !== 'LI') {
+async function createMarkup() {
+    try {
+        let arrayData = await getData();
+        if (!arrayData.length) {
             return
         }
-        let arrayData = getData();
-    
-        console.log(event.target.dataset.id)
+        listRef.innerHTML = murkup(arrayData);
+    } catch (error) {
+        { (error) => console.log(error) };
     }
+}
 
+function saveData(value) {
+    const time = Date.now();
+    const data = {
+        value,
+        approved: false,
+        id: time,
+    }
+    set(ref(db, 'listItems/' + time), data);   
+}
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        onValue(ref(db, PATH), (snapshot) => {
+            const data = snapshot.val();
+            
+            if (!data) {
+                (error) => reject(new Error); 
+            } else {
+                resolve(Object.values(data));
+            }
+        });
+    })
+}
+
+async function onListClick(event) {
+    if (event.target.tagName !== 'LI') {
+        return
+    }
+    let arrayData = await getData();
+    const item = getObject(arrayData, event.target.dataset.id);
+    item.approved = !item.approved;
+        update(ref(db, PATH + item.id), item);
+    createMarkup();
+}
+
+function getObject(arrayData, id) {
+    return arrayData.find(arrayItem => arrayItem.id === Number(id))
+}
+
+async function onClose(e) {
+    e.preventDefault();
+    if (e.target.tagName !== 'BUTTON') {
+        return
+    }
+    try {
+        let arrayData = await getData();
+        const icon = e.target.firstChild;
+        e.target.innerHTML = '';
+    } catch (error) {
+        console.log(error);
+    } 
+}   
+
+function deleteItem(item) {
+    remove(ref(db, PATH + item.id))
+}
---
+++ src/js/refs.js
@@ -0,0 +1,3 @@
+export const inputRef = document.querySelector("input");
+export const buttonRef = document.querySelector("button");
+export const listRef = document.querySelector("#myUL");
